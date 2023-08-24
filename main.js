3083b346dad1ae93be0443b78d2d8993dbd10f61
samuelebruni <samuele_bruni@hotmail.com>
2023-08-24 16:38:40 +0200

    proceed work

--- main.js
+++ main.js
@@ -16,21 +16,30 @@ va applicato uno sconto del 40% per gli over 65. */
 */
 
 
-
 document.getElementById("generate").addEventListener("click", function() {
-   let userIdentity = document.getElementById("user_identity").value;
-   let distanceKm = document.getElementById("distance").value;
-   let userAge = document.getElementById("age").value;
 
-   let ticketName = document.getElementById("ticket_name");
-   
-   
-   ticketName.innerHTML = userIdentity;
+    const priceForKm = 0.21;
+    const userIdentity = document.getElementById("user_identity").value;
+    const distanceKm = document.getElementById("distance").value;
+    const userAge = document.getElementById("age").value;
+    let priceTicket = priceForKm * distanceKm;
+    let salePrice;
 
-   
+    if (userAge === "2") {
+        salePrice = (priceTicket / 100) * 20;
+        //sconto per minorenne
+    } else if (userAge === "1") {
+        salePrice = (priceTicket / 100) * 40;
+        //sconto per over 65
+    } else {
+        salePrice = 0;
+        //nessuno sconto
+    }
 
+    const finalPrice = priceTicket - salePrice;
 
-   
+    console.log(priceForKm, userIdentity, distanceKm, userAge, priceTicket, salePrice, finalPrice);
+
   });
 
 
