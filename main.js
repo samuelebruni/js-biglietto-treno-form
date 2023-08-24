0d2c89449875d79cc4bcd00468f08b3b27b9411d
samuelebruni <samuele_bruni@hotmail.com>
2023-08-24 17:01:37 +0200

    process work

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ va applicato uno sconto del 40% per gli over 65. */
 
 document.getElementById("generate").addEventListener("click", function() {
 
+    //elenco variabili utili ai fini dei calcoli
     const priceForKm = 0.21;
     const userIdentity = document.getElementById("user_identity").value;
     const distanceKm = document.getElementById("distance").value;
@@ -25,10 +26,11 @@ document.getElementById("generate").addEventListener("click", function() {
     let priceTicket = priceForKm * distanceKm;
     let salePrice;
 
-    if (userAge === "2") {
+    //operazione per ottenere lo sconto
+    if (userAge === "Minorenne") {
         salePrice = (priceTicket / 100) * 20;
         //sconto per minorenne
-    } else if (userAge === "1") {
+    } else if (userAge === "Over65") {
         salePrice = (priceTicket / 100) * 40;
         //sconto per over 65
     } else {
@@ -36,9 +38,16 @@ document.getElementById("generate").addEventListener("click", function() {
         //nessuno sconto
     }
 
-    const finalPrice = priceTicket - salePrice;
+    //calcolo del prezzo finale
+    let finalPrice = Number (priceTicket - salePrice);
+    finalPrice = finalPrice.toFixed(2);
 
-    console.log(priceForKm, userIdentity, distanceKm, userAge, priceTicket, salePrice, finalPrice);
+    
+    console.log("Nome e Cognome:", userIdentity);
+    console.log("Distanza da Percorrere:", distanceKm, "KM");
+    console.log(userAge);
+    console.log("Sconto Applicato:", salePrice, "euro");
+    console.log("Prezzo Finale:", finalPrice, "euro");
 
   });
 
